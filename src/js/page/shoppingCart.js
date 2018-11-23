c93fb790d58fa0783da2c544a358b24edb9c1e8d
chui-web <158874645@qq.com>
2018-11-23 18:46:55 +0800

    添加 shoppingCart demo

--- src/js/page/shoppingCart.js
+++ src/js/page/shoppingCart.js
@@ -1,3 +1,25 @@
-/**
- * Created by SEELE on 2018/11/23.
- */
+import React , {Component} from 'react'
+import ReactDOM from 'react-dom';
+import './../../sass/reset.scss'
+import './../../sass/shoppingCart.scss'
+
+class TodoList  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            shoppingList:[]
+        };
+    }
+
+    render(){
+        return (
+            <div className="m-shopping-cart">
+
+            </div>
+        )
+    }
+}
+ReactDOM.render(
+    <TodoList/>,
+    document.getElementById('root')
+);
