da58443cfc2cf03ae8e6bc8796ad02ae0da67468
chui-web <158874645@qq.com>
2018-11-26 18:31:56 +0800

    整理 shoppingCart demo 的多余数据

--- src/js/page/shoppingCart.js
+++ src/js/page/shoppingCart.js
@@ -22,15 +22,15 @@ class ShoppingCart extends Component {
                 },
                 {
                     'pro_id': '',
-                    'pro_name': '【斯文】甘油 | 丙三醇',//产品名称
-                    'pro_brand': 'skc',//品牌名称
-                    'pro_place': '韩国',//产地
-                    'pro_purity': '99.7%',//规格
-                    'pro_min': "215千克",//最小起订量
-                    'pro_depot': '上海仓海仓储',//所在仓库
-                    'pro_num': 3,//数量
-                    'pro_img': '../../images/ucenter/testimg.jpg',//图片链接
-                    'pro_price': 800//单价
+                    'pro_name': '【斯文】甘油 | 丙三醇',
+                    'pro_brand': 'skc',
+                    'pro_place': '韩国',
+                    'pro_purity': '99.7%',
+                    'pro_min': "215千克",
+                    'pro_depot': '上海仓海仓储',
+                    'pro_num': 3,
+                    'pro_img': '../../images/ucenter/testimg.jpg',
+                    'pro_price': 800
                 }
             ]
         };
