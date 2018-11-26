ff748b0eb17735d2c96960d1745dd4bc98fcde77
chui-web <158874645@qq.com>
2018-11-26 18:01:17 +0800

    shoppingCart demo 完善数据和布局

--- src/js/page/shoppingCart.js
+++ src/js/page/shoppingCart.js
@@ -1,15 +1,15 @@
-import React , {Component} from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom';
 import './../../sass/reset.scss'
 import './../../sass/shoppingCart.scss'
 
-class ShoppingCart  extends Component {
+class ShoppingCart extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            shoppingList:[
+            shoppingList: [
                 {
-                    'pro_id':'',
+                    'pro_id': '',
                     'pro_name': '【斯文】甘油 | 丙三醇',//产品名称
                     'pro_brand': 'skc',//品牌名称
                     'pro_place': '韩国',//产地
@@ -21,7 +21,7 @@ class ShoppingCart  extends Component {
                     'pro_price': 800//单价
                 },
                 {
-                    'pro_id':'',
+                    'pro_id': '',
                     'pro_name': '【斯文】甘油 | 丙三醇',//产品名称
                     'pro_brand': 'skc',//品牌名称
                     'pro_place': '韩国',//产地
@@ -36,40 +36,44 @@ class ShoppingCart  extends Component {
         };
     }
 
-    render(){
+    render() {
         return (
             <div className="m-shopping-cart">
                 <table>
                     <tbody>
                     {
-                        this.state.shoppingList.map((item,index)=>{
+                        this.state.shoppingList.map((item, index) => {
                             return (
                                 <tr key={item.pro_id}>
-                                <td className="td-check"><span className="check-span"></span></td>
-                                <td className="td-product"><img src={item.pro_img} width="98" height="98"/>
-                                <div className="product-info">
-                                <h6>{item.pro_name}</h6>
-                            <p>品牌：{item.pro_brand}&nbsp;&nbsp;产地：{item.pro_place}</p>
-                            <p>规格/纯度:{item.pro_purity}&nbsp;&nbsp;起定量：{item.pro_min}</p>
-                            <p>配送仓储：{item.pro_depot}</p>
-                            </div>
-                            <div className ="clearfix"></div>
-                            </td>
-                            <td className="td-num">
-                                <div className="product-num">
-                                    <a href="javascript:;" className="num-reduce num-do fl" onClick={()=>{}}><span></span></a>
-                                        <input type="text" className="num-input" v-model="item.pro_num"/>
-                                        <a href="javascript:;" className="num-add num-do fr" onClick={()=>{}}><span></span></a>
+                                    <td className="td-check"><span className="check-span"></span></td>
+                                    <td className="td-product"><img src={item.pro_img} width="98" height="98"/>
+                                        <div className="product-info">
+                                            <h6>{item.pro_name}</h6>
+                                            <p>品牌：{item.pro_brand}&nbsp;&nbsp;产地：{item.pro_place}</p>
+                                            <p>规格/纯度:{item.pro_purity}&nbsp;&nbsp;起定量：{item.pro_min}</p>
+                                            <p>配送仓储：{item.pro_depot}</p>
                                         </div>
-                                        </td>
-                                        <td className="td-price">
-                                        <p className="red-text">￥<span className="price-text">{item.pro_price}</span></p>
-                                        </td>
-                                        <td className="td-total">
-                                        <p className="red-text">￥<span className="total-text">{item.pro_price*item.pro_num}</span>.00</p>
-                                        </td>
-                                        <td className="td-do"><a href="javascript:;" className="product-delect">删除</a></td>
-                                        </tr>
+                                        <div className="clearfix"></div>
+                                    </td>
+                                    <td className="td-num">
+                                        <div className="product-num">
+                                            <a href="javascript:;" className="num-reduce num-do fl" onClick={() => {
+                                            }}><span></span></a>
+                                            <input type="text" className="num-input" v-model="item.pro_num"/>
+                                            <a href="javascript:;" className="num-add num-do fr" onClick={() => {
+                                            }}><span></span></a>
+                                        </div>
+                                    </td>
+                                    <td className="td-price">
+                                        <p className="red-text">￥<span className="price-text">{item.pro_price}</span>
+                                        </p>
+                                    </td>
+                                    <td className="td-total">
+                                        <p className="red-text">￥<span
+                                            className="total-text">{item.pro_price * item.pro_num}</span>.00</p>
+                                    </td>
+                                    <td className="td-do"><a href="javascript:;" className="product-delect">删除</a></td>
+                                </tr>
                             )
                         })
                     }
