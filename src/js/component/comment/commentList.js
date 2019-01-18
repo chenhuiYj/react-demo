4259174eac58c7ab3261551d5239015a8a2127ee
chui-web <158874645@qq.com>
2019-01-18 17:21:25 +0800

    整理代码

--- src/js/component/comment/commentList.js
+++ src/js/component/comment/commentList.js
@@ -11,9 +11,7 @@ class CommentList extends Component {
                 {comment.commentList.length>0?<h3>评论列表</h3>:<p>暂无评论</p>}
                 {comment.commentList.map((item, index) => <div key={item.id}
                                                                className="m-comment-item"><p>{index + 1}.{item.content}</p>
-                    <a href="javascript:;">回复</a>
-
-                                                               </div>)}
+                    <a href="javascript:;">回复</a></div>)}
             </div>
         )
     }
