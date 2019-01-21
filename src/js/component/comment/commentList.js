c14a8e625a02e4276e3e914c65f04e201d124e11
chui-web <158874645@qq.com>
2019-01-21 19:14:36 +0800

    修复bug

--- src/js/component/comment/commentList.js
+++ src/js/component/comment/commentList.js
@@ -9,7 +9,7 @@ class CommentList extends Component {
         super(props);
         this.state = {
             applyContent:'',
-            isApply:false
+            isApply:-1
         };
     }
     handleBlur(e){
@@ -21,7 +21,7 @@ class CommentList extends Component {
     submitApply(i){
         this.setState({
             applyContent: '',
-            isApply:false
+            isApply:-1
         });
         let obj={
             con:this.state.applyContent,
@@ -34,10 +34,9 @@ class CommentList extends Component {
     /**
      * @description 回复
      */
-    switchApply(e) {
-        let _apply=!this.state.isApply;
+    switchApply(index) {
         this.setState({
-            isApply: _apply,
+            isApply: index
         });
     }
     render() {
@@ -46,9 +45,9 @@ class CommentList extends Component {
                 {comment.commentList.length>0?<h3>评论列表</h3>:<p>暂无评论</p>}
                 {comment.commentList.map((item, index) => <div key={item.id}
                                                                className="m-comment-item"><p>{index + 1}.{item.content}</p>
-                    {item.apply.map(sub=><div key={sub.id}>{sub.con}</div>)}
-                    {this.state.isApply?<div style={{paddingLeft:'20px'}}><input placeholder="请输入回复内容" value={this.state.applyContent} onChange={this.handleBlur.bind(this)}/><a href="javascript:;" onClick={()=>{this.submitApply(index)}}>确定</a><a href="javascript:;" onClick={this.switchApply.bind(this)}>取消</a></div>:<a href="javascript:;" onClick={this.switchApply.bind(this)}>回复</a>}
-
+                    {item.apply.length>0?<h4 style={{paddingLeft:'20px'}}>回复列表</h4>:''}
+                    {item.apply.map(sub=><div key={sub.id}  style={{paddingLeft:'20px'}}>{sub.con}</div>)}
+                    {this.state.isApply===item.id?<div><input placeholder="请输入回复内容" value={this.state.applyContent} onChange={this.handleBlur.bind(this)}/><a href="javascript:;" onClick={()=>{this.submitApply(index)}}>确定</a><a href="javascript:;" onClick={()=>{this.switchApply(-1)}}>取消</a></div>:<a href="javascript:;" onClick={()=>{this.switchApply(index)}}>回复</a>}
                     </div>)}
             </div>
         )
