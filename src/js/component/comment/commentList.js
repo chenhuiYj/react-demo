2f69d96de7416975e6ff15d9899d9d7cb7da824f
chui-web <158874645@qq.com>
2019-01-21 18:44:13 +0800

    编写回复功能

--- src/js/component/comment/commentList.js
+++ src/js/component/comment/commentList.js
@@ -5,13 +5,29 @@ import '../../../sass/commentList.scss'
 
 @observer
 class CommentList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isApply:false
+        };
+    }
+    /**
+     * @description 回复
+     */
+    switchApply(e) {
+        let _apply=!this.state.isApply;
+        this.setState({
+            isApply: _apply,
+        });
+    }
     render() {
         return (
             <div className="m-comment-list">
                 {comment.commentList.length>0?<h3>评论列表</h3>:<p>暂无评论</p>}
                 {comment.commentList.map((item, index) => <div key={item.id}
                                                                className="m-comment-item"><p>{index + 1}.{item.content}</p>
-                    <a href="javascript:;">回复</a></div>)}
+                    {this.state.isApply?<input placeholder="请输入回复内容"/>:<a href="javascript:;" onClick={this.switchApply.bind(this)}>回复</a>}
+                    </div>)}
             </div>
         )
     }
